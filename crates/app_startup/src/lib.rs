7e19a4478aafe3fa6b9ec65fbb50fc9775ab407e
lukaszmojek <lukasz.mojek@pm.me>
2024-03-03 10:22:10 +0100

    Propagate books data for displaying on UI using ECS in Bevy

--- crates/app_startup/src/lib.rs
+++ crates/app_startup/src/lib.rs
@@ -1,9 +1,10 @@
 use bevy::prelude::*;
+use ui;
 
 pub fn start_app() {
     App::new()
         .add_plugins(
-            DefaultPlugins.set(WindowPlugin {
+            (DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
                     resolution: [870., 1066.].into(),
                     resize_constraints: WindowResizeConstraints {
@@ -15,6 +16,8 @@ pub fn start_app() {
                     ..Default::default()
                 }),
                 ..Default::default()
-            }))
+            }),
+        ui::UserBooksPlugin))
         .run();
 }
+
