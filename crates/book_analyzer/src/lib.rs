d3ca86c970051c0bf93587689f3dc07721e81b30
lukaszmojek <lukasz.mojek@pm.me>
2024-03-05 20:34:07 +0100

    Add tests for different versions of EPUB format

--- crates/book_analyzer/src/lib.rs
+++ crates/book_analyzer/src/lib.rs
@@ -1,15 +1,24 @@
 use epub::doc::EpubDoc;
+use std::fs::File;
+use std::io::BufReader;
 
 #[derive(Debug)]
 pub struct BookDetails {
     pub title: String,
-    pub author: String
+    pub author: String,
+    pub file: EpubDoc<BufReader<File>>,
+}
+
+impl BookDetails {
+
+    pub fn get_spine(&self) -> Vec<String> {
+        self.file.spine.clone()
+    }
 }
 
 impl PartialEq for BookDetails {
     fn eq(&self, other: &Self) -> bool {
-        self.title == other.title &&
-            self.author == other.author
+        self.title == other.title && self.author == other.author
     }
 }
 
@@ -17,24 +26,19 @@ pub fn read_specific_book(book_path: String) -> Option<BookDetails> {
     let doc = EpubDoc::new(book_path);
 
     if doc.is_err() {
-        return None
+        return None;
     }
 
     let doc = doc.unwrap();
 
     //TODO: Improve metadata extraction from book, firstly by list of mappings
-    let title = match doc.mdata("title") {
-        None => {"Unknown".to_string()}
-        Some(title) => { title }
-    };
+    let title = doc.mdata("title").unwrap_or_else(|| "Unknown".to_string());
 
-    let author = match doc.mdata("creator") {
-        None => {"Unknown".to_string()},
-        Some(author) => { author }
-    };
+    let author = doc.mdata("creator").unwrap_or_else(|| "Unknown".to_string());
 
     Some(BookDetails {
         title,
-        author
+        author,
+        file: doc,
     })
 }
--- crates/book_analyzer/tests/read.rs
+++ crates/book_analyzer/tests/read.rs
@@ -1,19 +1,42 @@
-mod book_reading_tests {
+mod book_opening_tests {
+    use book_analyzer::{read_specific_book, BookDetails};
+    use epub::doc::EpubDoc;
     use rstest::rstest;
-    use book_analyzer::{BookDetails, read_specific_book};
 
     //DISCLAIMER: those books are free and downloaded from https://www.gutenberg.org/
     #[rstest]
-    #[case::the_wonderful_wizard_of_oz("the_wonderful_wizard_of_oz", "The Wonderful Wizard of Oz", "L. Frank Baum")]
-    #[case::the_time_machine("the_time_machine", "The Time Machine", "H. G. Wells")]
-    fn should_return_some_with_details_when_book_exists(#[case] file_name: String, #[case] title: String, #[case] author: String) {
+    #[case::the_wonderful_wizard_of_oz_epub3(
+        "the_wonderful_wizard_of_oz_v3",
+        "The Wonderful Wizard of Oz",
+        "L. Frank Baum"
+    )]
+    #[case::the_wonderful_wizard_of_oz_epub2(
+        "the_wonderful_wizard_of_oz_v2",
+        "The Wonderful Wizard of Oz",
+        "L. Frank Baum"
+    )]
+    #[case::the_wonderful_wizard_of_oz_v2_epub2_no_images(
+        "the_wonderful_wizard_of_oz_v2_no_images",
+        "The Wonderful Wizard of Oz",
+        "L. Frank Baum"
+    )]
+    fn should_return_some_with_details_when_book_exists(
+        #[case] file_name: String,
+        #[case] title: String,
+        #[case] author: String,
+    ) {
         let book_path = format!("./tests/data/{file_name}.epub");
-        let expected_details = BookDetails { title, author };
+        let expected_details = BookDetails {
+            title,
+            author,
+            file: EpubDoc::new(&book_path).unwrap(),
+        };
 
         let option_result = read_specific_book(book_path);
 
         assert!(option_result.is_some());
-        assert_eq!(option_result.unwrap(), expected_details);
+        let details = option_result.unwrap();
+        assert_eq!(details, expected_details);
     }
 
     #[test]
@@ -24,4 +47,56 @@ mod book_reading_tests {
 
         assert!(option_result.is_none());
     }
+
+    #[rstest]
+    #[case::the_wonderful_wizard_of_oz_v3("the_wonderful_wizard_of_oz_v3")]
+    #[case::the_wonderful_wizard_of_oz_v2("the_wonderful_wizard_of_oz_v2")]
+    #[case::the_wonderful_wizard_of_oz_v2_no_images("the_wonderful_wizard_of_oz_v2_no_images")]
+    fn toc_should_contain_list_of_all_sections(#[case] file_name: String) {
+        //arrange
+        let book_path = format!("./tests/data/{file_name}.epub");
+        let expected_sections = vec![
+            "The Wonderful Wizard of Oz",
+            "Contents",
+            "Introduction",
+            "The Wonderful Wizard of Oz",
+            "Chapter I The Cyclone",
+            "Chapter II The Council with the Munchkins",
+            "Chapter III How Dorothy Saved the Scarecrow",
+            "Chapter IV The Road Through the Forest",
+            "Chapter V The Rescue of the Tin Woodman",
+            "Chapter VI The Cowardly Lion",
+            "Chapter VII The Journey to the Great Oz",
+            "Chapter VIII The Deadly Poppy Field",
+            "Chapter IX The Queen of the Field Mice",
+            "Chapter X The Guardian of the Gate",
+            "Chapter XI The Wonderful City of Oz",
+            "Chapter XII The Search for the Wicked Witch",
+            "Chapter XIII The Rescue",
+            "Chapter XIV The Winged Monkeys",
+            "Chapter XV The Discovery of Oz, the Terrible",
+            "Chapter XVI The Magic Art of the Great Humbug",
+            "Chapter XVII How the Balloon Was Launched",
+            "Chapter XVIII Away to the South",
+            "Chapter XIX Attacked by the Fighting Trees",
+            "Chapter XX The Dainty China Country",
+            "Chapter XXI The Lion Becomes the King of Beasts",
+            "Chapter XXII The Country of the Quadlings",
+            "Chapter XXIII Glinda The Good Witch Grants Dorothy’s Wish",
+            "Chapter XXIV Home Again",
+            "THE FULL PROJECT GUTENBERG LICENSE",
+        ];
+
+        //act
+        let book_details = read_specific_book(book_path).unwrap();
+        let toc = book_details.file.toc;
+
+        //assert
+        assert_eq!(toc.len(), expected_sections.len());
+
+        for index in 0..toc.len() {
+            assert_eq!(expected_sections[index], toc[index].label);
+        }
+    }
 }
+
