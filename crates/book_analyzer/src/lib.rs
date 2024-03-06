041dad703bab165dc8e05a3c03e9d11e93afbd7c
lukaszmojek <lukasz.mojek@pm.me>
2024-03-06 21:42:23 +0100

    Save before switching to rbook instead of epub-rs

--- crates/book_analyzer/src/lib.rs
+++ crates/book_analyzer/src/lib.rs
@@ -1,4 +1,4 @@
-use epub::doc::EpubDoc;
+use epub::doc::{EpubDoc, NavPoint};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -6,11 +6,104 @@ use std::io::BufReader;
 pub struct BookDetails {
     pub title: String,
     pub author: String,
-    pub file: EpubDoc<BufReader<File>>,
+    file: EpubDoc<BufReader<File>>
 }
 
 impl BookDetails {
+    pub fn create_with_epubrs(book_path: String) -> Option<BookDetails> {
+        let doc = EpubDoc::new(book_path);
 
+        if doc.is_err() {
+            //TODO: Think how to handle errors
+            return None;
+        }
+
+        let doc = doc.unwrap();
+
+        //TODO: Improve metadata extraction from book, firstly by list of mappings, later can be improved
+        let title = doc.mdata("title").unwrap_or_else(|| "Unknown".to_string());
+        let author = doc.mdata("creator").unwrap_or_else(|| "Unknown".to_string());
+
+        Some(BookDetails {
+            title,
+            author,
+            file: doc,
+        })
+    }
+
+    pub fn create_with_rbook(book_path: String) -> Option<BookDetails> {
+        let doc = EpubDoc::new(book_path);
+
+        if doc.is_err() {
+            //TODO: Think how to handle errors
+            return None;
+        }
+
+        let doc = doc.unwrap();
+
+        //TODO: Improve metadata extraction from book, firstly by list of mappings, later can be improved
+        let title = doc.mdata("title").unwrap_or_else(|| "Unknown".to_string());
+        let author = doc.mdata("creator").unwrap_or_else(|| "Unknown".to_string());
+
+        Some(BookDetails {
+            title,
+            author,
+            file: doc,
+        })
+    }
+    
+    pub fn table_of_contents(&self) -> Vec<NavPoint> {
+        self.file.toc.clone()
+    }
+    
+    pub fn number_of_chapters(&self) -> usize {
+        self.file.get_num_pages()
+    }
+
+    pub fn get_chapter(&mut self, index: usize) -> Option<(String, String)> {
+        let chapter_nav_point = self.file.toc.get(index);
+
+        if chapter_nav_point.is_none() {
+            return None;
+        }
+        
+        let chapter_path = chapter_nav_point.unwrap().clone().content;
+        println!("{:?}", chapter_path);
+        let chapter_content = self.file.get_resource_str(chapter_path.to_str().unwrap());
+        
+        chapter_content
+    }
+
+    pub fn get_page(&mut self, index: usize) -> Option<(Vec<u8>, String)> {
+        // let was_page_changed = self.file.set_current_page(index);
+        // 
+        // if was_page_changed {
+        //     return None;
+        // }
+        let moved = self.file.go_next();
+        let moved = self.file.go_next();
+        let moved = self.file.go_next();
+        let moved = self.file.go_next();
+        let moved = self.file.go_next();
+        
+        let current_page = match self.file.get_current() {
+            None => {None}
+            Some(content) => {Some(content)}
+        };
+        
+        current_page 
+    }
+
+    pub fn get_book_contents(&mut self) -> Vec<&String> {
+        let mut book_contents: Vec<&String> = vec![];
+        
+        for content in self.file.spine.iter() {
+            book_contents.push(content);
+        }
+        
+        book_contents
+    }
+    
     pub fn get_spine(&self) -> Vec<String> {
         self.file.spine.clone()
     }
@@ -22,23 +115,3 @@ impl PartialEq for BookDetails {
     }
 }
 
-pub fn read_specific_book(book_path: String) -> Option<BookDetails> {
-    let doc = EpubDoc::new(book_path);
-
-    if doc.is_err() {
-        return None;
-    }
-
-    let doc = doc.unwrap();
-
-    //TODO: Improve metadata extraction from book, firstly by list of mappings
-    let title = doc.mdata("title").unwrap_or_else(|| "Unknown".to_string());
-
-    let author = doc.mdata("creator").unwrap_or_else(|| "Unknown".to_string());
-
-    Some(BookDetails {
-        title,
-        author,
-        file: doc,
-    })
-}
--- crates/book_analyzer/tests/read.rs
+++ crates/book_analyzer/tests/read.rs
@@ -1,6 +1,5 @@
-mod book_opening_tests {
-    use book_analyzer::{read_specific_book, BookDetails};
-    use epub::doc::EpubDoc;
+mod book_opening_tests_epubrs {
+    use book_analyzer::{BookDetails};
     use rstest::rstest;
 
     //DISCLAIMER: those books are free and downloaded from https://www.gutenberg.org/
@@ -22,28 +21,24 @@ mod book_opening_tests {
     )]
     fn should_return_some_with_details_when_book_exists(
         #[case] file_name: String,
-        #[case] title: String,
-        #[case] author: String,
+        #[case] expected_title: String,
+        #[case] expected_author: String,
     ) {
         let book_path = format!("./tests/data/{file_name}.epub");
-        let expected_details = BookDetails {
-            title,
-            author,
-            file: EpubDoc::new(&book_path).unwrap(),
-        };
 
-        let option_result = read_specific_book(book_path);
+        let option_result = BookDetails::create_with_epubrs(book_path);
 
         assert!(option_result.is_some());
         let details = option_result.unwrap();
-        assert_eq!(details, expected_details);
+        assert_eq!(details.title, expected_title);
+        assert_eq!(details.author, expected_author);
     }
 
     #[test]
     fn should_return_none_when_book_not_exists() {
         let book_path = "./tests/data/not_existing.epub".to_string();
 
-        let option_result = read_specific_book(book_path);
+        let option_result = BookDetails::create_with_epubrs(book_path);
 
         assert!(option_result.is_none());
     }
@@ -88,8 +83,8 @@ mod book_opening_tests {
         ];
 
         //act
-        let book_details = read_specific_book(book_path).unwrap();
-        let toc = book_details.file.toc;
+        let book_details = BookDetails::create_with_epubrs(book_path).unwrap();
+        let toc = &book_details.table_of_contents();
 
         //assert
         assert_eq!(toc.len(), expected_sections.len());
@@ -100,3 +95,157 @@ mod book_opening_tests {
     }
 }
 
+mod book_opening_tests_rbook {
+    use book_analyzer::{BookDetails};
+    use rbook::Ebook;
+    use rstest::rstest;
+
+    //DISCLAIMER: those books are free and downloaded from https://www.gutenberg.org/
+    #[rstest]
+    #[case::the_wonderful_wizard_of_oz_epub3(
+    "the_wonderful_wizard_of_oz_v3",
+    "The Wonderful Wizard of Oz",
+    "L. Frank Baum"
+    )]
+    fn should_return_some_with_details_when_book_exists(
+        #[case] file_name: String,
+        #[case] expected_title: String,
+        #[case] expected_author: String,
+    ) {
+        let book_path = format!("./tests/data/{file_name}.epub");
+        
+        let epub = rbook::Epub::new(&book_path).unwrap();
+        let mut reader = epub.reader();
+
+        while let Some(Ok(content)) = reader.next_page() {
+            println!("{content}")
+        }
+    }
+
+    #[test]
+    fn should_return_none_when_book_not_exists() {
+        let book_path = "./tests/data/not_existing.epub".to_string();
+
+        let option_result = BookDetails::create_with_rbook(book_path);
+
+        assert!(option_result.is_none());
+    }
+
+    #[rstest]
+    #[case::the_wonderful_wizard_of_oz_v3("the_wonderful_wizard_of_oz_v3")]
+    #[case::the_wonderful_wizard_of_oz_v2("the_wonderful_wizard_of_oz_v2")]
+    #[case::the_wonderful_wizard_of_oz_v2_no_images("the_wonderful_wizard_of_oz_v2_no_images")]
+    fn toc_should_contain_list_of_all_sections(#[case] file_name: String) {
+        //arrange
+        let book_path = format!("./tests/data/{file_name}.epub");
+        let expected_sections = vec![
+            "The Wonderful Wizard of Oz",
+            "Contents",
+            "Introduction",
+            "The Wonderful Wizard of Oz",
+            "Chapter I The Cyclone",
+            "Chapter II The Council with the Munchkins",
+            "Chapter III How Dorothy Saved the Scarecrow",
+            "Chapter IV The Road Through the Forest",
+            "Chapter V The Rescue of the Tin Woodman",
+            "Chapter VI The Cowardly Lion",
+            "Chapter VII The Journey to the Great Oz",
+            "Chapter VIII The Deadly Poppy Field",
+            "Chapter IX The Queen of the Field Mice",
+            "Chapter X The Guardian of the Gate",
+            "Chapter XI The Wonderful City of Oz",
+            "Chapter XII The Search for the Wicked Witch",
+            "Chapter XIII The Rescue",
+            "Chapter XIV The Winged Monkeys",
+            "Chapter XV The Discovery of Oz, the Terrible",
+            "Chapter XVI The Magic Art of the Great Humbug",
+            "Chapter XVII How the Balloon Was Launched",
+            "Chapter XVIII Away to the South",
+            "Chapter XIX Attacked by the Fighting Trees",
+            "Chapter XX The Dainty China Country",
+            "Chapter XXI The Lion Becomes the King of Beasts",
+            "Chapter XXII The Country of the Quadlings",
+            "Chapter XXIII Glinda The Good Witch Grants Dorothy’s Wish",
+            "Chapter XXIV Home Again",
+            "THE FULL PROJECT GUTENBERG LICENSE",
+        ];
+
+        //act
+        let book_details = BookDetails::create_with_rbook(book_path).unwrap();
+        let toc = book_details;
+
+        //assert
+        assert_eq!(toc.len(), expected_sections.len());
+
+        for index in 0..toc.len() {
+            assert_eq!(expected_sections[index], toc[index].label);
+        }
+    }
+}
+
+// mod book_traversing_tests {
+//     use book_analyzer::{BookDetails};
+//     use epub::doc::EpubDoc;
+//     use rstest::rstest;
+// 
+//     #[test]
+//     fn should_get_first_chapter() {
+//         let book_path = "./tests/data/the_wonderful_wizard_of_oz_v3.epub".to_string();
+//         let expected_text = "Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, who was a farmer";
+// 
+//         let mut book_details = BookDetails::create_with_epubrs(book_path).unwrap();
+//         let chapter = book_details.get_chapter(2);
+// 
+//         // let spine = book_details.get_spine();
+// 
+//         // let some_chapter = book_details.file.get_resource(spine[0].as_str());
+//         // let chapter_content = String::from_utf8(some_chapter.unwrap().0);
+// 
+//         // assert_eq!(spine.len(), 1);
+//         //
+//         assert!(chapter.is_some());
+//         println!("{:?}", expected_text);
+//         assert!(chapter.unwrap().0.contains(expected_text));
+//     }
+// 
+//     #[test]
+//     fn should_get_content_at_page() {
+//         let book_path = "./tests/data/the_wonderful_wizard_of_oz_v3.epub".to_string();
+//         let expected_text = "Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, who was a farmer";
+// 
+//         let mut book_details = BookDetails::create_with_epubrs(book_path).unwrap();
+//         let chapter = book_details.get_page(2);
+// 
+//         // let spine = book_details.get_spine();
+// 
+//         // let some_chapter = book_details.file.get_resource(spine[0].as_str());
+//         // let chapter_content = String::from_utf8(some_chapter.unwrap().0);
+// 
+//         // assert_eq!(spine.len(), 1);
+//         //
+//         assert!(chapter.is_some());
+//         println!("{:?}", String::from_utf8(chapter.unwrap().0));
+//         // assert!(chapter.unwrap()..contains(expected_text));
+//     }
+//     
+//     #[test]
+//     fn should_get_whole_book_contents() {
+//         let book_path = "./tests/data/the_wonderful_wizard_of_oz_v3.epub".to_string();
+//         let expected_text = "Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, who was a farmer";
+// 
+//         let mut book_details = BookDetails::create_with_epubrs(book_path).unwrap();
+//         let contents = book_details.get_book_contents();
+// 
+//         // let spine = book_details.get_spine();
+// 
+//         // let some_chapter = book_details.file.get_resource(spine[0].as_str());
+//         // let chapter_content = String::from_utf8(some_chapter.unwrap().0);
+// 
+//         // assert_eq!(spine.len(), 1);
+//         // assert!(chapter.unwrap()..contains(expected_text));
+// 
+//         for content in contents {
+//             println!("{:?}", content)
+//         }
+//     }
+// }
