cdaa83311c5bc6c2e4b70c2b03a2465a3ea61e56
lukaszmojek <lukasz.mojek@pm.me>
2024-03-08 10:07:05 +0100

    Implement searching for .opf file inside META-INF/container.xml

--- crates/epub/src/lib.rs
+++ crates/epub/src/lib.rs
@@ -2,6 +2,7 @@ use quick_xml::events::Event;
 use quick_xml::Reader;
 use std::fs::File;
 use std::io::Read;
+use quick_xml::name::QName;
 use zip::ZipArchive;
 
 fn read_epub() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,30 +23,42 @@ fn parse_container(zip: &mut ZipArchive<File>) -> Result<String, Box<dyn std::er
 
     let mut reader = Reader::from_str(&contents);
     reader.trim_text(true);
-    // let mut buf = Vec::new();
+
+    let mut txt = Vec::new();
+    let mut buf = Vec::new();
 
     // Find the OPF file path in the container XML
     let mut opf_path = String::new();
-    // loop {
-    //     match reader.read_event() {
-    //         Event::Start(ref e) if e.name() == b"rootfile" => {
-    //             for attribute in e.attributes() {
-    //                 let attr = attribute?;
-    //                 if attr.key == b"full-path" {
-    //                     opf_path = String::from_utf8(attr.value.into_owned())?;
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //         Event::Eof => break,
-    //         _ => (),
-    //     }
-    //     buf.clear();
-    // }
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(e) | Event::Empty(e)) => {
+                println!("{:?}", e);
+                match e.name().as_ref() {
+                    b"rootfile" => {
+                        for attribute in e.attributes() {
+                            let attr = attribute?;
+                            if attr.key == QName(b"full-path") {
+                                opf_path = String::from_utf8(attr.value.into_owned())?;
+                                break;
+                            }
+                        }
+                    },
+                    _ => (),
+                }
+            }
+            Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
+            _ => (),
+        }
+        buf.clear();
+    }
 
     if opf_path.is_empty() {
+        println!("ERR");
         Err("OPF file not found".into())
     } else {
+        println!("OK");
         Ok(opf_path)
     }
 }
@@ -67,7 +80,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let _ = read_epub();
+    fn parse_container_should_return_path_to_opf() {
+        let epub_file = File::open("./data/moby-dick.epub").unwrap();
+        let mut archive = ZipArchive::new(epub_file).unwrap();
+
+        let opf_path = parse_container(&mut archive);
+        
+        assert!(opf_path.is_ok());
+        assert_eq!(opf_path.unwrap(), "OPS/package.opf")
     }
 }
