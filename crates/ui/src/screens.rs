99b04b5efc92a59ee36b2da2ae86678d2bad309f
lukaszmojek <lukasz.mojek@pm.me>
2024-03-20 19:39:12 +0100

    Transition between Splash screen and Home screen

--- crates/ui/src/screens.rs
+++ crates/ui/src/screens.rs
@@ -61,6 +61,7 @@ pub mod splash {
                     ..default()
                 });
             });
+
         // Insert the timer as a resource
         commands.insert_resource(SplashTimer(Timer::from_seconds(1.0, TimerMode::Once)));
     }
@@ -78,7 +79,7 @@ pub mod splash {
 }
 
 pub mod home {
-    use super::{despawn_screen, LoreLeafState};
+    use super::{despawn_screen, LoreLeafState, TEXT_COLOR};
     use bevy::prelude::*;
 
     pub struct HomePlugin;
@@ -92,18 +93,44 @@ pub mod home {
         LoreExplorer,
     }
 
+    // State used for the current menu screen
+    #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+    enum MenuState {
+        Main,
+        Settings,
+        SettingsDisplay,
+        SettingsSound,
+        #[default]
+        Disabled,
+    }
+
     impl Plugin for HomePlugin {
         fn build(&self, app: &mut App) {
             app.init_state::<NavigationState>()
+                .add_systems(OnEnter(NavigationState::Library), menu_setup)
                 .add_systems(OnEnter(LoreLeafState::Home), home_setup)
                 .add_systems(Update, countdown.run_if(in_state(LoreLeafState::Home)))
-                .add_systems(OnExit(LoreLeafState::Home), despawn_screen::<OnHomeScreen>);
+                .add_systems(OnExit(LoreLeafState::Home), despawn_screen::<OnHomeScreen>)
+                .init_state::<MenuState>()
+                .add_systems(OnEnter(MenuState::Main), main_menu_setup);
         }
     }
 
     // Tag component used to tag entities added on the home screen
     #[derive(Component)]
     struct OnHomeScreen;
+
+    // All actions that can be triggered from a button click
+    #[derive(Component)]
+    enum MenuButtonAction {
+        Play,
+        Settings,
+        SettingsDisplay,
+        SettingsSound,
+        BackToMainMenu,
+        BackToSettings,
+        Quit,
+    }
 
     // Newtype to use a `Timer` for this screen as a resource
     #[derive(Resource, Deref, DerefMut)]
@@ -146,11 +173,113 @@ pub mod home {
     ) {
         println!("in HOME")
     }
+
+    fn menu_setup(mut menu_state: ResMut<NextState<MenuState>>) {
+        menu_state.set(MenuState::Main);
+    }
+
+    fn main_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+        // Common style for all buttons on the screen
+        let button_style = Style {
+            width: Val::Px(250.0),
+            height: Val::Px(65.0),
+            margin: UiRect::all(Val::Px(20.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        };
+        let button_icon_style = Style {
+            width: Val::Px(30.0),
+            // This takes the icons out of the flexbox flow, to be positioned exactly
+            position_type: PositionType::Absolute,
+            // The icon will be close to the left border of the button
+            left: Val::Px(10.0),
+            ..default()
+        };
+        let button_text_style = TextStyle {
+            font_size: 40.0,
+            color: TEXT_COLOR,
+            ..default()
+        };
+
+        commands
+            .spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                },
+                //TODO: Should probably be navigation
+                OnHomeScreen,
+            ))
+            .with_children(|parent| {
+                parent
+                    .spawn(NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        background_color: BackgroundColor::from(Color::CRIMSON),
+                        ..default()
+                    })
+                    .with_children(|parent| {
+                        // Display the game name
+                        parent.spawn(
+                            TextBundle::from_section(
+                                "Nav",
+                                TextStyle {
+                                    font_size: 80.0,
+                                    color: TEXT_COLOR,
+                                    ..default()
+                                },
+                            )
+                            .with_style(Style {
+                                margin: UiRect::all(Val::Px(50.0)),
+                                ..default()
+                            }),
+                        );
+
+                        // Display three buttons for each action available from the main menu:
+                        // - new game
+                        // - settings
+                        // - quit
+                        parent
+                            .spawn((
+                                ButtonBundle {
+                                    style: button_style.clone(),
+                                    // image: UiImage::default().with_color(NORMAL_BUTTON),
+                                    ..default()
+                                },
+                                MenuButtonAction::Play,
+                            ))
+                            .with_children(|parent| {
+                                let icon = asset_server.load("textures/Game Icons/right.png");
+                                parent.spawn(ImageBundle {
+                                    style: button_icon_style.clone(),
+                                    image: UiImage::new(icon),
+                                    ..default()
+                                });
+                                parent.spawn(TextBundle::from_section(
+                                    "New Game",
+                                    button_text_style.clone(),
+                                ));
+                            });
+                    });
+            });
+    }
 }
 
 // Tag component used to mark which setting is currently selected
 #[derive(Component)]
 struct SelectedOption;
+
+const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
 const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
