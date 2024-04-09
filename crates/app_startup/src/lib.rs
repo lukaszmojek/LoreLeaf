9edfae9e8c53200a2e48ce1a5b9388f9cf634e02
lukaszmojek <lukasz.mojek@pm.me>
2024-04-09 06:35:36 +0200

    Change font to ReemKufiFun to support latin characters

--- crates/app_startup/src/lib.rs
+++ crates/app_startup/src/lib.rs
@@ -1,11 +1,12 @@
-use bevy::{prelude::*, winit::WinitSettings};
+use bevy::{asset::load_internal_binary_asset, prelude::*, winit::WinitSettings};
 use ui::screens::LoreLeafState;
 
 const LORE_LEAF_TITLE: &str = "LoreLeaf";
 
 pub fn start_app() {
-    App::new()
-        .init_state::<LoreLeafState>()
+    let mut app = App::new();
+
+    app.init_state::<LoreLeafState>()
         // TODO: Think how to turn that off for development
         // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
         // .insert_resource(WinitSettings::desktop_app())
@@ -26,8 +27,19 @@ pub fn start_app() {
             }),
             ui::SplashPlugin,
             ui::HomePlugin,
-        ))
-        .run();
+        ));
+
+    // This needs to happen after `DefaultPlugins` is added.
+    // It is an workaround used for replacing font with custom one and was mentioned here: https://github.com/bevyengine/bevy/discussions/11839
+    // Other option would be the need of specyfying font each time a TextBundle is used, just like it was done here: https://github.com/bevyengine/bevy/pull/9259/files
+    load_internal_binary_asset!(
+        app,
+        TextStyle::default().font,
+        "../../../assets/fonts/ReemKufiFun-Regular.ttf",
+        |bytes: &[u8], _path: String| { Font::try_from_bytes(bytes.to_vec()).unwrap() }
+    );
+
+    app.run();
 }
 
 fn setup(mut commands: Commands) {
